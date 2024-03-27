7af21819c1e656fa186e8485e87c6b1e11e22a6b
PhuocTai <tai.ngo1773@gmail.com>
2024-03-27 21:51:47 +0700

    check getter, setter

--- src/main/java/com/btv/app/author/Author.java
+++ src/main/java/com/btv/app/author/Author.java
@@ -14,13 +14,30 @@ public class Author {
     public Author() {}
 
     public Author(Author tmp) {
-        this.id = tmp.getID();
-        this.name = tmp.getName();
+        this.id = tmp.id;
+        this.name = tmp.name;
     }
 
-    public long getID() {return this.id;}
-    public String getName() {return this.name;}
+//    public long getID() {return this.id;}
+//    public String getName() {return this.name;}
+//
+//    void setId(long ID) {this.id = ID;}
+//    void setName(String NAME) {this.name = NAME;}
 
-    void setId(long ID) {this.id = ID;}
-    void setName(String NAME) {this.name = NAME;}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
