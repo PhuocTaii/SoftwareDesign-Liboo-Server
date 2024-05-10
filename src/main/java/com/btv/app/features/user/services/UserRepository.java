bc1f454a7099e34e57edb6799d2c2af848fca0e4
Van Tran <thanhvan26823@gmail.com>
2024-05-10 13:33:18 +0700

    filter user accounts by role

--- src/main/java/com/btv/app/features/user/services/UserRepository.java
+++ src/main/java/com/btv/app/features/user/services/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -34,4 +35,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Page<User> findByNameContainsAndRoleAndStatusAllIgnoreCase(String name, Role role, Boolean status, Pageable pageable);
 
+    Page<User> findByEmailContainsIgnoreCase(String email, Pageable pageable);
 }
